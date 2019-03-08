f6dc703a00de17738f8af24c7f192a7b462154e4
ThaDocIzN <rodan72@msn.com>
2019-03-07 23:22:14 -0600

    added order model

---
+++ app/src/main/java/com/thadocizn/frontendshoppingcart/model/Order.java
@@ -0,0 +1,41 @@
+package com.thadocizn.frontendshoppingcart.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import lombok.Data;
+
+@Data
+public class Order {
+
+    @SerializedName("order_id")
+    private long orderId;
+
+    @SerializedName("product_name")
+    private String productName;
+
+    @SerializedName("shipping_address")
+    private String shippingAddress;
+
+    @SerializedName("payment_details")
+    private String paymentDetails;
+
+    @SerializedName("has_shipped")
+    private boolean hasShipped;
+
+    @SerializedName("product_quantity")
+    private int productQuantity;
+
+    public Order(long orderId,
+                 String productName,
+                 String shippingAddress,
+                 String paymentDetails,
+                 boolean hasShipped,
+                 int productQuantity) {
+        this.orderId = orderId;
+        this.productName = productName;
+        this.shippingAddress = shippingAddress;
+        this.paymentDetails = paymentDetails;
+        this.hasShipped = hasShipped;
+        this.productQuantity = productQuantity;
+    }
+}
