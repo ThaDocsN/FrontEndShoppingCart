e32c498b6a54349801df0efde8fa4c5f9f6a0283
ThaDocIzN <rodan72@msn.com>
2019-03-07 22:53:23 -0600

    added product model

--- app/src/main/java/com/thadocizn/frontendshoppingcart/model/Product.java
+++ app/src/main/java/com/thadocizn/frontendshoppingcart/model/Product.java
@@ -1,4 +1,37 @@
 package com.thadocizn.frontendshoppingcart.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import lombok.Data;
+
+@Data
 public class Product {
+
+    @SerializedName("product_id")
+    private long productId;
+
+    @SerializedName("product_name")
+    private String productName;
+
+    @SerializedName("product_description")
+    private String productDescription;
+
+    @SerializedName("price")
+    private double price;
+
+    @SerializedName("quantity")
+    private int quantity;
+
+    public Product(long productId,
+                   String productName,
+                   String productDescription,
+                   double price,
+                   int quantity) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
 }
